81bdb4c680011670f1e8673b76ce7125a2e446f7
Muhammad-Saad0 <119536957+Muhammad-Saad0@users.noreply.github.com>
2023-07-25 20:40:17 +0500

    added chainlinkVRF to generate random numbers

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,18 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-error Raffle__NotEnoughEthEntered();
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
-contract Raffle {
+error Raffle__NotEnoughEthEntered();
+error Raffle__TransferFailed();
+
+contract Raffle is VRFConsumerBaseV2 {
     /*State Variable*/
     uint256 private immutable i_enteranceFee;
     address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_VRFCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private immutable i_callbackGasLimit;
+    uint32 private constant NUM_WORDS = 1;
+
+    address private s_recentWinner;
 
     /*Events */
     event RaffleEntered(address indexed player);
+    event RequestedRaffleWinner(uint256 indexed requestId);
+    event WinnerPicked(address indexed winner);
 
-    constructor(uint256 enteranceFee) {
+    constructor(
+        uint256 enteranceFee,
+        address _VRFCoordinator,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(_VRFCoordinator) {
         i_enteranceFee = enteranceFee;
+        i_VRFCoordinator = VRFCoordinatorV2Interface(_VRFCoordinator);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() public payable {
@@ -23,11 +47,48 @@ contract Raffle {
         emit RaffleEntered(msg.sender);
     }
 
+    //This function uses chainlink VRF
+    function requestRandomWinner() external returns (uint256 requestId) {
+        /*key hash will determine max gas price
+         we willing to pay to get the random
+         numbers*/
+
+        /*callback gas limit basically
+        determines how much gas can
+        fulfillRandomWords function can take */
+        requestId = i_VRFCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        emit RequestedRaffleWinner(requestId);
+    }
+
+    function fulfillRandomWords(
+        uint256 /*requestId*/,
+        uint256[] memory randomWords
+    ) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+        emit WinnerPicked(recentWinner);
+    }
+
     function getEnteranceFee() public view returns (uint256) {
         return i_enteranceFee;
     }
 
     function getPlayer(uint256 index) public view returns (address) {
         return s_players[index];
+    }
+
+    function getRecentWinner() public view returns (address) {
+        return s_recentWinner;
     }
 }
