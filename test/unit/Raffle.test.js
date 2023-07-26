e73961d3e4f0a8159ec353716b357ea53aa0b696
Muhammad-Saad0 <119536957+Muhammad-Saad0@users.noreply.github.com>
2023-07-27 02:06:02 +0500

    wrote tests for upkeep functionality

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -42,6 +42,7 @@ console.log(network.name);
           //setting the raffle state to false
 
           await raffle.enterRaffle({ value: sendETH });
+
           let currentTimestamp = (await ethers.provider.getBlock())
             .timestamp;
           const nextBlockTimeStamp =
@@ -49,6 +50,7 @@ console.log(network.name);
           await ethers.provider.send("evm_setNextBlockTimestamp", [
             nextBlockTimeStamp,
           ]);
+          //moving to the new block to get the updated timestamp
           await ethers.provider.send("evm_mine");
 
           //performing upkeep to set Raffel State to false
@@ -56,6 +58,83 @@ console.log(network.name);
           await expect(
             raffle.enterRaffle({ value: sendETH })
           ).to.be.revertedWith("Raffle__LotteryNotOpen");
+
+          //checking if the event was emitted
+        });
+
+        it("checks if the event was emitted", async () => {
+          const tx = await raffle.enterRaffle({ value: sendETH });
+          const reciept = await tx.wait(1);
+
+          const player = reciept.events[0].args.player;
+          expect(player).to.equal(deployer);
+        });
+
+        it("checks if the player array was updated successfully", async () => {
+          const tx = await raffle.enterRaffle({ value: sendETH });
+
+          const player = await raffle.getPlayer(0);
+          expect(player).to.equal(deployer);
+        });
+      });
+
+      describe("testing checkUpKeep method", () => {
+        it("returns false if we dont have any balance", async () => {
+          //MAKING SURE THE INTERVAL HAS PASSED
+          let currentTimestamp = (await ethers.provider.getBlock())
+            .timestamp;
+          const nextBlockTimeStamp =
+            currentTimestamp + Number(interval) + 1;
+          await ethers.provider.send("evm_setNextBlockTimestamp", [
+            nextBlockTimeStamp,
+          ]);
+          //moving to the new block to get the updated timestamp
+          await ethers.provider.send("evm_mine");
+
+          const { upkeepNeeded } = await raffle.callStatic.checkUpkeep([]);
+          expect(upkeepNeeded).to.equal(false);
+        });
+
+        it("returns false if not enough time passed", async () => {
+          //we are using callStatic because we need return values
+          //we dont actually want to send a transaction
+          const { upkeepNeeded } = await raffle.callStatic.checkUpkeep([]);
+          expect(upkeepNeeded).to.equal(false);
+        });
+      });
+
+      describe("Testing performUpKeep method", () => {
+        beforeEach(async () => {
+          await raffle.enterRaffle({ value: sendETH });
+        });
+
+        describe("testing when enough time has passed", () => {
+          beforeEach(async () => {
+            let currentTimestamp = (await ethers.provider.getBlock())
+              .timestamp;
+            const nextBlockTimeStamp =
+              currentTimestamp + Number(interval) + 1;
+            await ethers.provider.send("evm_setNextBlockTimestamp", [
+              nextBlockTimeStamp,
+            ]);
+            //moving to the new block to get the updated timestamp
+            await ethers.provider.send("evm_mine");
+          });
+
+          it("emits an event when succesfull", async () => {
+            //performing upkeep to set Raffel State to false
+            const tx = await raffle.performUpkeep([]);
+            const reciept = await tx.wait(1);
+            const requestId = reciept.events[1].args.requestId;
+
+            expect(requestId.toString()).to.be.equal("1");
+          });
+
+          it("Updates raffle state to calculating", async () => {
+            await raffle.performUpkeep([]);
+            const raffleState = await raffle.getRaffleState();
+            expect(Number(raffleState)).to.equal(1);
+          });
         });
       });
     });
